e6fbfc350b57f839709ed70f978f7b67a76fc019
BnGalal <45380168+BnGalal@users.noreply.github.com>
2023-12-27 10:13:40 +0300

    Update xfce4.sh
    adding Bluetooth packages

--- xfce4.sh
+++ xfce4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies gvfs dbus
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies gvfs dbus bluez bluez-utils blueman
 sudo systemctl enable lightdm
-sudo pacman -S firefox kazam vlc
+sudo systemctl enable bluetooth.service
+sudo pacman -S firefox vlc
+
+# below line to use bluetooth after login 1st time
+# sudo usermod -aG lp $(whoami)
+# blueman-manager
