dcab8ed2f73591a84a9f199c81e10f5ad00ffa74
BnGalal <45380168+BnGalal@users.noreply.github.com>
2023-12-31 10:01:59 +0300

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -3,7 +3,7 @@
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies gvfs dbus bluez bluez-utils blueman
 sudo systemctl enable lightdm
 sudo systemctl enable bluetooth.service
-sudo pacman -S firefox vlc
+sudo pacman -S firefox vlc xarchiver xz unrar unzip p7zip
 
 # below line to use bluetooth after login 1st time
 # sudo usermod -aG lp $(whoami)
