63f6014c20fb3341aeb01e7c0469d0d30511bab1
BnGalal <45380168+BnGalal@users.noreply.github.com>
2023-12-31 10:02:14 +0300

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S sddm plasma kde-applications packagekit-qt5 firefox vlc
+sudo pacman -S sddm plasma kde-applications packagekit-qt5 firefox vlc xarchiver xz unrar unzip p7zip
 sudo systemctl enable sddm
